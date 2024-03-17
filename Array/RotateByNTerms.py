21403e35e0b219578422f9b8f42acffb3293500d
Vishal-m <vishalmahesh.study@gmail.com>
2024-03-17 13:09:34 +0530

    array/ left and right rotate by N elements -optimal

--- Array/RotateByNTerms.py
+++ Array/RotateByNTerms.py
@@ -26,21 +26,54 @@ def optimal_solution_left(s, e, ls):
 
 
 
+# T.C = O(d) + O(N-d) + O(d) => O(N+d)
+# S.C = O(d)
+def brute_force_right(ls):
+    temp = ls[len(ls) - d:]
+    for i in range(len(ls)-1, d-1, -1):
+        ls[i] = ls[i - d]
+        # ls[i+d] = ls[i]
+
+    ls = temp + ls[d:]
+
+    return ls
+
+def optimal_right(ls, d):
+    # for i in range(len(ls)-1, len(ls)-d, -1):
+    ls[len(ls)-d:] = ls[len(ls)-d:][::-1]
+    ls[:len(ls)-d] = ls[:len(ls)-d][::-1]
+    ls.reverse()
+
+    return ls
+
+def optimal_left_slicing(ls, d):
+    ls[:d] = ls[:d][::-1]
+    ls[d:] = ls[d:][::-1]
+    ls.reverse()
+    return ls
 
 
 
-ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-d = 3
 
+
+
+ls1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ls2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+d = 2
+
+# print(ls[len(ls) - d: ])
+# print(brute_force_right(ls))
 # print(brute_force(ls, d))
+print(optimal_right(ls1, d), "right")
+print(optimal_left_slicing(ls2, d), "left")
 
-# optimal_solution(0, d-1, ls)
-# optimal_solution(d, len(ls) - 1, ls)
-# # optimal_solution(0, len(ls) - 1, ls)
+# for i in range(len(ls) -1, d-1, -1):
+#     print(i)
+
 
 # In-built-reverse
 # ls[:d] = ls[:d][::-1]
 # ls[d:] = ls[d:][::-1]
 # ls.reverse()
-#
 # print(ls)
+
