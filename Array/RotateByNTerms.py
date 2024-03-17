21ed04ee60838e5d3a4411398603397dec97ab29
Vishal-m <vishalmahesh.study@gmail.com>
2024-03-17 12:12:23 +0530

    array/ left rotate D terms

--- Array/RotateByNTerms.py
+++ Array/RotateByNTerms.py
@@ -1,4 +1,6 @@
-def brute_force(ls, d):
+# T.C => O(d) + (N-d) + O(d) => O(N+d)
+# S.C => O(d)
+def brute_force_left(ls, d):
     temp = []
     for i in range(d):
         temp.append(ls[i])
@@ -11,7 +13,34 @@ def brute_force(ls, d):
 
     return ls
 
+# T.C = O(d) + o(N-d) + O(N) => O(2N)
+# S.C = O(1)
+def optimal_solution_left(s, e, ls):
+    while s < e:
+        temp = ls[s]
+        ls[s] = ls[e]
+        ls[e] = temp
+
+        s+=1
+        e-=1
+
+
+
+
+
+
 ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 d = 3
 
-print(brute_force(ls, d))
+# print(brute_force(ls, d))
+
+# optimal_solution(0, d-1, ls)
+# optimal_solution(d, len(ls) - 1, ls)
+# # optimal_solution(0, len(ls) - 1, ls)
+
+# In-built-reverse
+# ls[:d] = ls[:d][::-1]
+# ls[d:] = ls[d:][::-1]
+# ls.reverse()
+#
+# print(ls)
