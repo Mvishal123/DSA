d6d583bba10d68314ef002290a94ea82d3dcee31
Vishal-m <vishalmahesh.study@gmail.com>
2024-03-18 16:20:01 +0530

    Arrays/ Intersection of sorted arrays -optimal solution

--- Array/UnionAndIntersection.py
+++ Array/UnionAndIntersection.py
@@ -1,5 +1,5 @@
-arr1 = [1, 3, 3, 3, 3, 4, 5, 7]
-arr2 = [2, 3, 5, 6, 7, 7, 7, 7, 7, 7, 7]
+arr1 = [1, 1, 2, 3, 8, 9, 10]
+arr2 = [2, 8]
 
 # Output
 # condition -> arrays should be sorted
@@ -60,15 +60,35 @@ def intersection_method_1(ls1, ls2):
                     if j != last:
                         final.append(i)
                         last = i
-
-
-
     return final
 
 
-print(union_method_1(arr1, arr2))
-print(intersection_method_1(arr1, arr2))
+def intersection_optimal_method(ls1, ls2):
+    # check which arr is less in size and start with it, but we won't be doing that here.
 
+    final = []
+    i, j = 0, 0
+
+    while i < len(ls1) and j < len(ls2):
+        # As the array is sorted no need to check the other elements also
+        if ls1[i] < ls2[j]:
+            i+=1
+        elif ls1[i] > ls2[j]:
+            j+=1
+        else:
+            final.append(ls1[i])
+            i+=1
+            j+=1
+    return final
+
+
+
+
+
+
+print(union_method_1(arr1, arr2))
+# print(intersection_method_1(arr1, arr2))
+print(intersection_optimal_method(arr1, arr2))
 # s1 = set(arr1).union(arr2)
 # s2 = set(arr1).intersection(arr2)
 # ls1 = []
